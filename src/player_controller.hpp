499ebd83b85aa328ae7a2b39e2347dfb33d4eb2e
Luke <luke.lukejoe@gmail.com>
2024-11-11 20:15:18 -0800

    Refactored player code to clean up world system

---
+++ src/player_controller.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "common.hpp"   // Include any necessary headers
+#include <GLFW/glfw3.h> // Include GLFW for key and mouse input
+
+class PlayerController
+{
+public:
+    PlayerController();
+
+    void step(float elapsed_ms_since_last_update);
+
+    // Handle input
+    void on_key(int key, int, int action, int mods);
+    void on_mouse_move(vec2 mouse_position);
+    void on_mouse_button(int button, int action, int mod);
+
+    void set_player_reference(Entity *player);
+    void set_renderer(RenderSystem *renderer);
+
+private:
+    Entity *my_player; // Pointer to the player instance to control
+    RenderSystem *renderer;
+};
